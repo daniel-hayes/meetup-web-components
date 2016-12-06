b3a1ebed2f80285fb3186b2cfc62c06f827e18d4
Michael McGahan <mike.mcgahan@gmail.com>
2016-12-07 09:49:31 +1300

    story with two buttons, labeled actions

--- src/togglepill.story.jsx
+++ src/togglepill.story.jsx
@@ -2,31 +2,52 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import TogglePill from './TogglePill';
 
-const onChange = e => {
-	action(`The value of the Toggle Pill clicked is: ${e.target.value}`)(e);
+const onChange = label => e => {
+	action(`The value of the Toggle Pill ${label} clicked is: ${e.target.value}`)(e);
 };
 
 
 storiesOf('TogglePill', module)
 	.add('Default', () => (
 		<TogglePill
-			onChange={onChange}
+			onChange={onChange('OFF`')}
 			id='togglePillId'
 			name='togglePillName'
 			value='toggle-pill'>Toggle Pill Label</TogglePill>
 	))
 	.add('Default Selected', () => (
 		<TogglePill
-			onChange={onChange}
+			onChange={onChange('ON')}
 			id='togglePillId'
 			name='togglePillName'
 			value='toggle-pill'
-			checked>Toggle Pill Label</TogglePill>
+			checked
+		>
+			Toggle Pill Label
+		</TogglePill>
+	))
+	.add('Pair of pills - action should be unique', () => (
+		<div>
+			<TogglePill
+				onChange={onChange('START_OFF')}
+				id='togglePillId'
+				name='togglePillName'
+				value='toggle-pill'>Toggle Pill Label</TogglePill>
+
+			<br />
+
+			<TogglePill
+				onChange={onChange('START_ON')}
+				id='togglePillId'
+				name='togglePillName'
+				value='toggle-pill'
+				checked>Toggle Pill Label</TogglePill>
+		</div>
 	))
 	.add('Topic Pill', () => (
 		<TogglePill
 			topic
-			onChange={onChange}
+			onChange={onChange('TOPIC_START_OFF')}
 			id='togglePillId'
 			name='togglePillName'
 			value='toggle-pill'>Toggle Pill Label</TogglePill>
@@ -34,7 +55,7 @@ storiesOf('TogglePill', module)
 	.add('Topic Pill Selected', () => (
 		<TogglePill
 			topic
-			onChange={onChange}
+			onChange={onChange('TOPIC_START_ON')}
 			id='togglePillId'
 			name='togglePillName'
 			value='toggle-pill'
