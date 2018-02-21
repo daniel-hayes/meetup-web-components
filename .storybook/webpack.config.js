d5762150c3d4cfb372e37c365b5d456ff2102bfe
Adam Detrick <akdetrick@gmail.com>
2018-02-21 14:12:41 -0500

    set correct order for postcss plugins

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,32 +1,8 @@
 const path = require('path');
 
 const SCSS_PATH = path.resolve(__dirname, '../assets', 'scss');
-const CSS_PATH = path.resolve(__dirname, '../assets', 'css');
 const SRC_PATH = path.resolve(__dirname, '../src');
 const PLATFORM_PATH = /node_modules\/meetup-web-platform/;
-
-const OPTS_POSTCSS = {
-	ident: 'postcss',
-	plugins: (loader) => [
-		require('postcss-cssnext')({
-			browsers: [
-				'last 2 versions',
-				'not ie <= 10'
-			],
-			features: {
-				customProperties: false
-			}
-		}),
-		require('postcss-css-variables')({
-			preserve: true
-		})
-	]
-};
-
-// placeholder for modules options
-// modules: true,
-// localIdentName: '_[name]_[local]__[hash:base64:5]',
-const OPTS_CSS = {};
 
 module.exports = {
 	module: {
@@ -38,32 +14,35 @@ module.exports = {
 				include: SRC_PATH
 			},
 			{
-				test: /\.css$/,
-				include: CSS_PATH,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {...OPTS_CSS,...{importLoaders: 1}},
-					},
-					{
-						loader: 'postcss-loader',
-						options: OPTS_POSTCSS
-					},
-				]
-			},
-			{
 				test: /\.scss$/,
 				include: SCSS_PATH,
 				use: [
 					'style-loader',
 					{
 						loader: 'css-loader',
-						options: {...OPTS_CSS,...{importLoaders: 2}},
+						options: {
+							importLoaders: 2,
+						},
 					},
 					{
 						loader: 'postcss-loader',
-						options: OPTS_POSTCSS
+						options: {
+							ident: 'postcss',
+							plugins: (loader) => [
+								require('postcss-css-variables')({
+									preserve: true
+								}),
+								require('postcss-cssnext')({
+									browsers: [
+										'last 2 versions',
+										'not ie <= 10'
+									],
+									features: {
+										customProperties: false
+									}
+								})
+							]
+						},
 					},
 					'sass-loader',
 				]
